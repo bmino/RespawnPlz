194ccbe4ec725c0f0777a3ca079b9fc04b0b3087
Brandon <brandonmino@bellsouth.net>
2021-01-10 21:38:48 -0500

    WiP

---
+++ RespawnPlz/ChatHelper.cs
@@ -0,0 +1,9 @@
+﻿using RoR2;
+
+namespace RespawnPlz {
+    public static class ChatHelper {
+        public static void sendBroadcastChat(string message) {
+            Chat.SendBroadcastChat(new Chat.SimpleChatMessage { baseToken = message });
+        }
+    }
+}
---
+++ RespawnPlz/RespawnPlz.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using RoR2;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Networking;
+
+namespace RespawnPlz {
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.bmino.RespawnPlz", "RespawnPlz", "1.0.0")]
+    public class RespawnPlz : BaseUnityPlugin {
+        public void Awake() {
+            Logger.LogMessage("Loaded RespawnPlz!");
+
+            On.RoR2.Console.RunCmd += (On.RoR2.Console.orig_RunCmd orig, RoR2.Console self, RoR2.Console.CmdSender sender, string concommandName, List<string> userArgs) => {
+                if (concommandName == "say" && userArgs.FirstOrDefault() == "respawnplz") {
+                    if (!isHost()) {
+                        Logger.LogMessage("Skipping respawn logic on non-host");
+                        return;
+                    }
+                    if (isDead(sender.networkUser)) {
+                        Logger.LogMessage("Cannot respawn a dead guy");
+                        return;
+                    }
+                    CharacterMaster master = sender.networkUser.master;
+                    ChatHelper.sendBroadcastChat($"That idiot, {sender.networkUser.userName}, is stuck again...");
+                    Stage.instance.RespawnCharacter(master); 
+                } else {
+                    orig(self, sender, concommandName, userArgs);
+                }
+            };
+        }
+
+        public bool isHost() {
+            return NetworkServer.active && Stage.instance != null;
+        }
+
+        public bool isDead(NetworkUser player) {
+            return !player.master.hasBody;
+        }
+    }
+}
